ffed8c7dcbc29697561382d2f123ce3a247811f4
lichuang <101010.l>
2019-10-09 16:25:02 +0800

    变更访问地址为http://localhost:8011/hello/2(num)

--- src/main/java/com/example/pachong/controller/HelloWorld.java
+++ src/main/java/com/example/pachong/controller/HelloWorld.java
@@ -1,6 +1,7 @@
 package com.example.pachong.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,18 +11,29 @@ import java.util.Map;
 /**
  * @author admin
  * @ClassName HelloWorld
- * @Description
+ * @Description 项目测试
  * @Date 2019/10/9
  */
 @RestController
 @RequestMapping("/hello")
 public class HelloWorld {
 
-  @GetMapping
-  public Map<String,Object> test(){
-    Map<String, Object> map = new HashMap<>();
-    map.put("name","lovec");
-    map.put("sex",'n');
+  private static String[] namesChar=new String[]{"aa","bb","cc","dd","ee","ff","gg","hh","ii","jj"};
+  private static String[] sex=new String[]{"nan","nv"};
+
+
+  /**
+   * 根据id来动态返回一个json串
+   *
+   * @param id id
+   * @return map
+   */
+  @GetMapping("/{id}")
+  public Map<String, Object> test(@PathVariable int id) {
+    Map<String, Object> map=new HashMap<>();
+    map.put("name",namesChar[id % 10]);
+    map.put("sex",sex[id % 2]);
+    map.put("age",id);
     return map;
   }
 }
